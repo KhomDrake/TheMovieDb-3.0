2add3939c723b123a2c47a7683dbe7cd66ebcc1e
KhomDrake <vinicius-de-viana@hotmail.com>
2023-06-11 00:31:19 -0300

    Imperiya Sample

---
+++ imperiyasample/src/main/java/com/vlv/imperiyasample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vlv.imperiyasample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
