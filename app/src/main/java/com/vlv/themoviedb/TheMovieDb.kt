0691add9b495a6b4e8d3244ad68a654b17d79fb9
KhomDrake <vinicius-de-viana@hotmail.com>
2023-06-25 23:43:16 -0300

    [THEM-15] Adding library startup

--- app/src/main/java/com/vlv/themoviedb/TheMovieDb.kt
+++ app/src/main/java/com/vlv/themoviedb/TheMovieDb.kt
@@ -1,24 +1,11 @@
 package com.vlv.themoviedb
 
 import android.app.Application
-import com.vlv.movie.ui.search.SearchViewModel
-import com.vlv.network.networkModule
-import com.vlv.themoviedb.ui.movie.favorites.MovieFavoritesViewModel
-import com.vlv.themoviedb.ui.movie.nowplaying.NowPlayingViewModel
-import com.vlv.themoviedb.ui.movie.trending.TrendingNowViewModel
+import androidx.startup.AppInitializer
+import com.vlv.themoviedb.ui.MainInitializer
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
-
-val appModule = module {
-    viewModel { NowPlayingViewModel(get()) }
-    viewModel { MovieFavoritesViewModel() }
-    viewModel { TrendingNowViewModel(get()) }
-    viewModel { com.vlv.movie.ui.TrendingNowViewModel(get()) }
-    viewModel { SearchViewModel(get()) }
-}
 
 class TheMovieDb : Application() {
 
@@ -31,10 +18,10 @@ class TheMovieDb : Application() {
         startKoin {
             androidLogger()
             androidContext(this@TheMovieDb)
-            modules(
-                networkModule + appModule
-            )
         }
+
+        AppInitializer.getInstance(this)
+            .initializeComponent(MainInitializer::class.java)
     }
 
 }
---
+++ network/src/main/kotlin/com/vlv/network/NetworkInitializer.kt
@@ -0,0 +1,34 @@
+package com.vlv.network
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.vlv.network.client.OkHttpFactory
+import com.vlv.network.interceptors.InterceptorFactory
+import com.vlv.network.moshi.MoshiFactory
+import com.vlv.network.repository.MovieRepository
+import com.vlv.network.retrofit.RetrofitFactory
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+class NetworkInitializer : Initializer<Module> {
+
+    override fun create(context: Context): Module {
+        val module = module {
+            single { MoshiFactory.moshi() }
+            single { InterceptorFactory() }
+            single { OkHttpFactory.create(get()) }
+            single { RetrofitFactory.retrofit(get(), get()) }
+            single { RetrofitFactory.service(get()) }
+
+            single { MovieRepository(get()) }
+        }
+        loadKoinModules(module)
+        return module
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+
+}
