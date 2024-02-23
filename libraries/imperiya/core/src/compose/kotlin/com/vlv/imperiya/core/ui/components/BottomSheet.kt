e4956355d1365759c0c731fdb42531fdf6457cd9
KhomDrake <vinicius-de-viana@hotmail.com>
2024-02-22 21:08:47 -0300

    Settings - Adding more previews

--- libraries/imperiya/core/src/compose/kotlin/com/vlv/imperiya/core/ui/components/BottomSheet.kt
+++ libraries/imperiya/core/src/compose/kotlin/com/vlv/imperiya/core/ui/components/BottomSheet.kt
@@ -9,17 +9,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
-import com.vlv.imperiya.core.ui.theme.TheMovieDbTypography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,6 +29,8 @@ fun TheMovieDbModalBottomSheet(
         modifier = modifier,
     ) {
         Column {
+            Spacer(modifier = Modifier
+                .navigationBarsPadding())
             content.invoke()
             Spacer(modifier = Modifier
                 .navigationBarsPadding())
